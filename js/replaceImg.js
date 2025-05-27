01f542e466028b1fbc410feef37e0923dc53ef82
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-05-27 19:52:35 -0300

    fixes: Finish

--- js/replaceImg.js
+++ js/replaceImg.js
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
    const button = document.querySelector('[data-fetch]')
    const imagemOriginal = './assets/images/icon-moon.svg'
    const novaImagem = './assets/images/icon-sun.svg'
+   const title = document.getElementById('title')
 
 botao.addEventListener('click', function() {
         if (imagem.getAttribute('src') === imagemOriginal) {
             imagem.src = novaImagem;
             fundo.classList.toggle('activeGroup')
             button.classList.add('activeGroup')
+            title.style.color = 'white'
         } else {
             imagem.src = imagemOriginal;
             button.classList.remove('activeGroup')
+            title.style.color = 'hsl(227, 75%, 14%)'
         }
     });
 
