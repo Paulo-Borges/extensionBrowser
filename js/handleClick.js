17f156c0d065c83931c370262cb788b0e10060b3
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-05-27 19:15:58 -0300

    fixes: Update handleClick

--- js/handleClick.js
+++ js/handleClick.js
@@ -1,24 +1,19 @@
 export default function handleClick() {
 
+    const all = document.querySelector('[data-todos]')
+    const active = document.querySelector('[data-ativo]')
+    const inativo = document.querySelector('[data-inativos]')
+    console.log(all, active, inativo)
+
+    all.addEventListener('click', click)
+    active.addEventListener('click', click)
+    inativo.addEventListener('click', click)
+
+    function click(event) {
+        event.preventDefault()
+        event.currentTarget.classList.toggle('active')
+        event.currentTarget.classList.toggle('choice-button')
+    
+    }
 }
 
-const all = document.querySelector('[data-todos]')
-const active = document.querySelector('[data-ativo]')
-const inativo = document.querySelector('[data-inativos]')
-console.log(all, active, inativo)
-
-all.addEventListener('click', click)
-active.addEventListener('click', click)
-inativo.addEventListener('click', click)
-
-function click(event) {
-    event.preventDefault()
-    all.classList.add('active')
-    all.classList.remove('choice-button')
-    active.classList.add('active')
-    active.classList.remove('choice-button')
-    inativo.classList.add('active')
-    inativo.classList.remove('choice-button')
-    console.log(event)
-
-}
